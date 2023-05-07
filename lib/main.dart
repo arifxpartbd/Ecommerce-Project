c71107126bf18c0561ace1293061a3b798f00516
arifxpartbd <arifkhancsebd@gmail.com>
2023-05-07 15:33:52 +0600

    version

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter/material.dart';
+import 'ui/screens/splash_screen.dart';
+
+void main(){
+  runApp(const CraftyBay());
+}
+
+class CraftyBay extends StatelessWidget {
+  const CraftyBay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+
+}
